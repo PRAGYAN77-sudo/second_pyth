a0d849bcd5e5d1319a710d3fc64aeb68f821604c
PRAGYAN77-sudo <pragyanmisra2010@gmail.com>
2026-08-22 09:37:12 +0400

    second commit

---
+++ Triangle.py
@@ -0,0 +1,3 @@
+b=int(input("Enter base of triangle"))
+h=int(input("Enter height of triangle"))
+print("Area of triangle is",1/2*b*h)
---
+++ celcius.py
@@ -0,0 +1,2 @@
+c=int(input("Enter the temp in celcius"))
+print("your temp in farenheit is",c*9/5+32)
